b65780c6c68fe6996570d62bfef8e95bac1d398e
Qurani Shifa <c050422018@gmail.com>
2024-01-01 22:59:47 -0800

    init

---
+++ app/Models/Subject.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Subject as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Subject extends Model
+{
+    use HasApiTokens, HasFactory;
+
+    protected $fillable = [
+        'title',
+        'lecturer_id',
+        'semester',
+        'academic_year',
+        'sks',
+        'code',
+        'description',
+
+    ];
+    
+    public function lecturer()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; simpadu-c050422018 2023
+  </div>
